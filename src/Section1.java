4686b043b3085f495a2008edca170a6c9ec0e709
Pedro Blascke <pedro-lucasm@hotmail.com>
2023-06-15 21:40:53 -0300

    Primeiro e único commit

---
+++ src/Section1.java
@@ -0,0 +1,6 @@
+public class Section1 {
+    public static void main(String[] args) throws Exception {
+        String saudacao = ("Olá, mundo!"); //Atividade 1
+        System.out.println(saudacao);
+    }
+}
